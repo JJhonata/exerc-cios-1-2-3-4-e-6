e8d771f3fcada33a3a2d5ee36e2b287d0299064c
Jhonata Vieira <astayunocorinthians@gmail.com>
2024-09-13 17:25:39 -0300

    Add files via upload

---
+++ atividade3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void imprima_Numeros(int N) {
+    if (N < 0) {
+        return;
+    }
+
+    imprima_Numeros(N - 1);
+
+    printf("%d ", N);
+}
+
+int main() {
+    int N;
+
+    printf("Diga um numero inteiro  para N: ");
+    scanf("%d", &N);
+
+    printf("Numeros de 0 até %d em ordem crescente: ", N);
+    imprima_Numeros(N);
+    printf("\n");
+
+    return 0;
+}
+
